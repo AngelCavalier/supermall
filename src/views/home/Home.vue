2af85a93c29479a2fd19457a65fb6c34a5fb2220
hexowei <hexowei@tencent.com>
2021-07-31 21:33:01 +0800

    feat:封装TabControl组件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,18 +3,33 @@
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control
+      class="tab-control"
+      :titles="['流行', '新款', '精选']"
+    ></tab-control>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar.vue";
+import TabControl from "components/content/tabControl/TabControl.vue";
+
 import HomeSwiper from "./childComps/HomeSwiper.vue";
 import RecommendView from "./childComps/RecommendView.vue";
+import FeatureView from "./childComps/FeatureView.vue";
+
 import { getHomeMultidata } from "network/home";
 
 export default {
   name: "Home",
-  components: { NavBar, HomeSwiper, RecommendView },
+  components: {
+    NavBar,
+    TabControl,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+  },
   data() {
     return {
       banners: [],
@@ -36,5 +51,13 @@ export default {
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+
+  position: sticky;
+  top: 0;
+  z-index: 1;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
